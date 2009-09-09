043be4abe8f4d0d19e738287a58a7653ef059327
Ian Whyman <ian.whyman@sabayonlinux.org>
2009-09-10 00:41:39 +0100

    [5.0] Change make_tarballs.sh to use BZ2, instead of LZMA

--- 5.0/make_tarballs.sh
+++ 5.0/make_tarballs.sh
@@ -5,9 +5,9 @@ FOLDERS="core gnome kde extra"
 mkdir /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}
 for tarball in ${FOLDERS}; do
 	echo "Compressing sabayon-artwork-${tarball}"
-	tar -c --file=sabayon-artwork-${tarball}.tar sabayon-artwork-${tarball}
-	lzma -9v -F lzma -S .lzma sabayon-artwork-${tarball}.tar
+	tar -cj --file=sabayon-artwork-${tarball}.tar.bz2 sabayon-artwork-${tarball}
+#	lzma -9v -F lzma -S .lzma sabayon-artwork-${tarball}.tar
 	echo "Moving sabayon-artwork-${tarball}"
-	mv sabayon-artwork-${tarball}.tar.lzma /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}/sabayon-artwork-${tarball}-${VERSION}.tar.lzma
+	mv sabayon-artwork-${tarball}.tar.bz2 /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
 done
 	
