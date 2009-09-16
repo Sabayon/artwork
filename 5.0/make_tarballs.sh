73092359df3eaed27c77ad50a8a824161a8464fc
Ian Whyman <ian.whyman@sabayonlinux.org>
2009-09-16 20:48:53 +0100

    [5.0/installer] Fix Installer Image

--- 5.0/make_tarballs.sh
+++ 5.0/make_tarballs.sh
@@ -5,9 +5,9 @@ FOLDERS="core gnome kde extra"
 mkdir /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}
 for tarball in ${FOLDERS}; do
 	echo "Compressing sabayon-artwork-${tarball}"
-	tar -cj --file=sabayon-artwork-${tarball}.tar.bz2 sabayon-artwork-${tarball}
+	tar -cj --file=sabayon-artwork-${tarball}.tbz2 sabayon-artwork-${tarball}
 #	lzma -9v -F lzma -S .lzma sabayon-artwork-${tarball}.tar
 	echo "Moving sabayon-artwork-${tarball}"
-	mv sabayon-artwork-${tarball}.tar.bz2 /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
+	mv sabayon-artwork-${tarball}.tbz2 /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
 done
 	
