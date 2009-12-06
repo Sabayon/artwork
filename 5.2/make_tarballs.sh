f5ad337ec0ab4fa9647235d5bf93c66c91af1b88
Ian Whyman <ian.whyman@sabayonlinux.org>
2009-12-06 22:49:27 +0000

    [5.2] Import folder structure from 5.0

---
+++ 5.2/make_tarballs.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+VERSION=${1}
+FOLDERS="core gnome kde extra"
+
+mkdir /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}
+for tarball in ${FOLDERS}; do
+	echo "Compressing sabayon-artwork-${tarball}"
+	tar -cj --file=sabayon-artwork-${tarball}.tbz2 sabayon-artwork-${tarball}
+#	lzma -9v -F lzma -S .lzma sabayon-artwork-${tarball}.tar
+	echo "Moving sabayon-artwork-${tarball}"
+	mv sabayon-artwork-${tarball}.tbz2 /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
+done
+	
