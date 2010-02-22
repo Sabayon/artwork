5a9e4b3fc718b2b338b97538ec82a60174186c9f
Ian Whyman <ian.whyman@sabayon.org>
2010-02-22 20:17:45 +0000

    [5.2/*] Prepare 5.2 Beta1

--- 5.2/misc/temp/gensplash.sh
+++ 5.2/misc/temp/gensplash.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 if [ -z "$1" ]; then
-    echo "Ian Whymans Automagical Image Resizing Thingie v1.0"
+    echo "Thev00d00's Automagical Image Resizing Thingie v2.0"
     echo ""
-    echo "usage: $0 fbsplash input output aspect(normal/wide/all)"
-    echo "usage: $0 ksplash input"
+    echo "usage: $0 fbsplash input output aspect(normal/wide/all) format"
+    echo "usage: $0 ksplash input format"
     exit
 fi
 
@@ -21,37 +21,40 @@ RES7="1280x800"
 RES8="1440x900"
 RES9="1680x1050"
 RES10="1920x1200"
+RES11="1024x600"
 
 if [ $1 = fbsplash ]
 then
     if [ $4 = normal ]
         then
-        RESES="$RES1 $RES2 $RES3 $RES4 $RES5 $RES6" 
+		#convert $2 -crop '1600x1200!+160' $2-43.png
+        RESES="$RES1 $RES2 $RES3 $RES4 $RES5 $RES6"
     elif [ $4 = wide ]
         then
-        RESES="$RES7 $RES8 $RES9 $RES10"
+        RESES="$RES7 $RES8 $RES9 $RES10 $RES11"
     elif [ $4 = all ]
         then
-        RESES="$RES1 $RES2 $RES3 $RES4 $RES5 $RES6 $RES7 $RES8 $RES9 $RES10"
+        RESES="$RES1 $RES2 $RES3 $RES4 $RES5 $RES6 $RES7 $RES8 $RES9 $RES10 $RES11"
     else
         echo "please choose normal, wide or all apsect ratios"
     fi
-        
-    echo "$RESES"
+
+    echo "Creating Images size: "$RESES""
     for i in ${RESES}
     do
-        convert $2 -filter Lanczos -resize ${i}\! $3-${i}.png
-        echo "converting ${2} to $2-${i}.png"
+        convert $2 -filter Lanczos -quality 94 -resize ${i}\! $3-${i}.${5}
+        echo "converting ${2} to ${3}-${i}.${5}"
     done
 
 elif [ $1 = ksplash ]
     then
-    for i in $RES1 $RES2 $RES3 $RES4 $RES6 $RES10
+    for i in 640x400 $RES2 $RES3 $RES4 $RES6 $RES10
     do
         mkdir $i
-        convert $2 -filter Lanczos -resize $i\! $i/background.png
-        echo "converting ${2} to $i/background.png"
-done
+        convert $2 -filter Lanczos -quality 94 -resize $i\! $i/background.${3}
+        echo "converting ${2} to $i/background.${3}"
+	done
+	convert $2 -filter Lanczos -resize 400x300\! Preview.png
 
 fi
 
