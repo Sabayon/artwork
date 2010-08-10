40a1a87e310c25ad554cf1f8bd2fdf2ca0f33193
Ian Whyman <ian.whyman@sabayon.org>
2010-08-10 23:08:13 +0100

    [scripts] Improve make_tarballs.sh a bit, add upload via rsync/ssh

--- 5.4/make_tarballs.sh
+++ 5.4/make_tarballs.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 VERSION=${1}
 FOLDERS="core gnome kde extra ooo lxde"
+FILENAME="sabayon-artwork-${tarball}-${VERSION}"
+LOCALDIR="/home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}"
+REMOTEHOST="ian@sabayon.org"
+REMOTEDIR="/home/sabayonlinux/public_html/distfiles.sabayon.org/x11-themes"
 
 mkdir /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}
 for tarball in ${FOLDERS}; do
@@ -8,6 +12,10 @@ for tarball in ${FOLDERS}; do
 	tar -cj --file=sabayon-artwork-${tarball}.tbz2 sabayon-artwork-${tarball}
 #	lzma -9v -F lzma -S .lzma sabayon-artwork-${tarball}.tar
 	echo "Moving sabayon-artwork-${tarball}"
-	mv sabayon-artwork-${tarball}.tbz2 /home/v00d00/build/sabayon/artwork-ztarballs/${VERSION}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
+	mv sabayon-artwork-${tarball}.tbz2 ${LOCALDIR}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
+	
+	#Upload
+	rsync --progress -ave ssh ${LOCALDIR}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2 \
+		${REMOTEHOST}:${REMOTEDIR}/sabayon-artwork-${tarball}/sabayon-artwork-${tarball}-${VERSION}.tar.bz2
 done
 
